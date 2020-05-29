65113bd959fe77ce193b6beb833745d07d5897b3
jesusrv1103 <jesus21c@hotmail.com>
2020-05-29 11:48:01 -0500

    necesario

--- app/Http/Controllers/MedidasDeProteccion/MedidaDeProteccionController.php
+++ app/Http/Controllers/MedidasDeProteccion/MedidaDeProteccionController.php
@@ -14,6 +14,7 @@ use App\Admin\Ocupacion;
 use App\Admin\Servicio;
 use App\Admin\Usuario;
 use Mail;
+use App\Mail\MedidaDeProteccionRecibida;
 use App\MedidasDeProteccion\Carpeta;
 use App\MedidasDeProteccion\Involucrado;
 Use App\MedidasDeProteccion\MedidaProteccion;
@@ -71,7 +72,7 @@ class MedidaDeProteccionController extends Controller
        //json_decode($request->input('municipio_carpeta'))->nombre;
 
      
-       Mail::to('jramirezv@fiscaliazacatecas.gob.mx')->send(new DenunciaRecibida($request));
+       Mail::to('jramirezv@fiscaliazacatecas.gob.mx')->send(new MedidaDeProteccionRecibida($request));
 
         
        return redirect()->back()->with('flash', 'Se ha recibido su predenuncia, personal de la Fiscalía se pondrá en contacto contigo vía correo electrónico para dar respuesta e indicar el trámite conducente.');;
