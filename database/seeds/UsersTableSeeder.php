616be77d9f6f42c92c0d7dd54eb7d7f944585ef7
Jesus Ramirez Vargas <jesus21c.jrv@gmail.com>
2020-06-09 13:13:28 -0500

    necesario

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,7 +16,7 @@ class UsersTableSeeder extends Seeder
             'name' => 'JESUS  RAMIREZ VARGAS',
             'user_name' => 'jramirezv',
             'email' => 'jramirezv@fiscaliazacatecas.gob.mx',
-            'password' => Hash::make('ravj931103f33'),
+            'password' => Hash::make('SDGY669H0ME7'),
             'tipo_usuario_id' => 1,
             'area_id'   => 1,
             'activo' => 1,
